c84058b14c27600d2c21c828d6a48707cca2a1af
EMALOK <emanuele.riccilucchi@gmail.com>
2022-02-26 16:38:59 +0100

    token enum init

---
+++ include/uvm_token.h
@@ -0,0 +1,14 @@
+// range 0 - 127
+enum uvm_token {
+    NULL = 0,
+    HEADER = 1,
+    BODY = 2,
+    LIST = 3,
+    STRING = 4,
+    VAR = 5,
+    FUNCTION = 6,
+    STRUCT = 7,
+    CLASS = 8,
+    PRIVATE = 9,
+    PUBLIC = 10,
+};
