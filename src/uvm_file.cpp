66fcbf35fe1d99215f5f4521228e363fa7bdfd47
EMALOK <emanuele.riccilucchi@gmail.com>
2022-02-26 09:54:35 +0100

    start parse head/body

--- src/uvm_file.cpp
+++ src/uvm_file.cpp
@@ -20,6 +20,8 @@ uvm_file::uvm_file(char* _file_path){
     infile.read((char*)raw_bytes, buffer_size);
 
     parse_version();
+
+    parse_file();
 }
 
 void uvm_file::parse_version(){
@@ -28,4 +30,23 @@ void uvm_file::parse_version(){
     uvm_version = raw_bytes[0] | (raw_bytes[1] << 8) | (raw_bytes[2] << 16) | (raw_bytes[3] << 24);
 
     std::cout << "UVM version: " << uvm_version << std::endl;
+}
+
+void uvm_file::parse_file(){
+
+    //switch based on the file version
+    std::cout << "starting parsing file using uvm version: " << uvm_version << std::endl;
+
+    switch (uvm_version)
+    {
+    case 0:
+    case 1:
+        break;
+    
+    default:
+        // not implemented or wrong uvm version
+        std::cout << "the version detected on the file does not match any uvm version in this VM" << std::endl;
+        break;
+    }
+
 }
